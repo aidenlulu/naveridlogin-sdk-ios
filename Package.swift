09142062ba0ebe5484cafc6cab49b0276c01296e
aidenlulu <sungsoo.yang@lulumedic.com>
2024-01-30 11:15:35 +0900

    feat: Product 추가

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "naveridlogin-sdk-ios",
+    products: [
+        .library(name: "NaverIdLoginLulu",
+                 targets: ["NaverThirdPartyLogin"])
+    ],
     targets: [
         .binaryTarget(name: "NaverThirdPartyLogin", path: "NaverThirdPartyLogin.xcframework")
     ]
